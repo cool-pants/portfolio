36d4fe5363cf32c80f36866435cbfdb35febc4d0
cool-pants <muban.sg@gmail.com>
2022-09-22 19:42:43 +0530

    [feat]: rehaul repo with nnp starter as base

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <n-page-header>
+        <template #title>
+            Mainak Sengupta
+        </template>
+        <template #avatar>
+            <n-avatar src="@/assets/coolpants.png" />
+        </template>
+    </n-page-header>
+</template>
+<script setup lang="ts">
+import { NPageHeader, NAvatar } from 'naive-ui'
+</script>
+<style lang="scss">
+.n-page-header {
+    padding-top: 5px;
+    padding-left: 5px;
+}
+</style>
