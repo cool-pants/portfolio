bc35a18dba65b99861b2cd1dc24afd1c6be0065f
cool-pants <muban.sg@gmail.com>
2022-10-03 01:00:22 +0530

    [feat]: add Intro and Projects data to main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,19 @@
 <template>
-    <div>
-        <Header />
-        <n-result status="success" title="succss" description="Naive-ui + Nuxt rc1">
-            <template #footer>
-                <n-space justify="center">
-                    <n-button type="success">Naive-ui</n-button>
-                    <n-button type="success" secondary>+</n-button>
-                    <n-button type="success">Nuxt rc1</n-button>
-                </n-space>
-            </template>
-        </n-result>
-        <n-space>
-            <n-button type="primary">naive-ui</n-button>
-            <n-button type="info">naive-ui</n-button>
-            <n-button type="warning">naive-ui</n-button>
-        </n-space>
-        <n-space>
-            <n-tag>Nuxt3</n-tag>
-            <n-tag>Vite</n-tag>
-            <n-tag>Naive-ui</n-tag>
-            <n-tag>SEO friendly</n-tag>
-        </n-space>
+    <div class="index">
+        <BaseComponent>
+            <IntroComponent></IntroComponent>
+            <ProjectComponent></ProjectComponent>
+            <div class="text" id="thir">Hello 3</div>
+        </BaseComponent>
     </div>
 </template>
 
 <script setup lang="ts">
-import Header from '~~/components/HeaderComponent.vue';
+import BaseComponent from '~~/components/BaseComponent.vue';
 </script>
+
+<style lang="scss">
+.text {
+    padding: 500px 100px;
+}
+</style>
