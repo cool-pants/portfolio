790a9daae659b3029e4b55aca266fb01f57b8a9a
cool-pants <muban.sg@gmail.com>
2022-10-03 00:59:29 +0530

    [feat]: add basic component abstraction

---
+++ components/BaseComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <Header />
+    <SidebarComponent>
+        <slot></slot>
+    </SidebarComponent>
+</template>
+
+<script setup lang="ts">
+import Header from '~~/components/HeaderComponent.vue';
+import SidebarComponent from '~~/components/SidebarComponent.vue';
+</script>
--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -4,7 +4,8 @@
             Mainak Sengupta
         </template>
         <template #avatar>
-            <n-avatar src="@/assets/coolpants.png" />
+            <n-avatar color="#fff"
+                src="https://raw.githubusercontent.com/cool-pants/portfolio/7fe043f2a954903a28c249d6673b4826da21e920/assets/coolpants.svg" />
         </template>
     </n-page-header>
 </template>
@@ -13,6 +14,10 @@ import { NPageHeader, NAvatar } from 'naive-ui'
 </script>
 <style lang="scss">
 .n-page-header {
+    position: fixed;
+    z-index: 1;
+    background-color: white;
+    width: 100%;
     padding-top: 5px;
     padding-left: 5px;
 }
