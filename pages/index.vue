58371f77104a106add2696f80b1e603798607b3a
cool-pants <muban.sg@gmail.com>
2022-10-03 01:10:31 +0530

    [feat]: add under construction flag

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,28 @@
         <BaseComponent>
             <IntroComponent></IntroComponent>
             <ProjectComponent></ProjectComponent>
-            <div class="text" id="thir">Hello 3</div>
+            <div class="text" id="thir">
+                <n-icon>
+                    <construct-outline />
+                </n-icon>
+                UNDER CONSTRUCTION
+            </div>
         </BaseComponent>
     </div>
 </template>
 
 <script setup lang="ts">
 import BaseComponent from '~~/components/BaseComponent.vue';
+import { ConstructOutline } from "@vicons/ionicons5";
 </script>
 
 <style lang="scss">
 .text {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    font-size: 60px;
     padding: 500px 100px;
 }
 </style>
